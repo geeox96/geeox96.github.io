10944d79eee414f28928ca08dc9cf3d189aabc90
Geovânio Carnelosso <geovanio.carnelosso@arctica.one>
2021-10-08 11:42:27 -0300

    new website

---
+++ main.js
@@ -0,0 +1,37 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true; // Auto scroll
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector('.current');
+  current.classList.remove('current');
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add('current');
+  } else {
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+next.addEventListener('click', e => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+prev.addEventListener('click', e => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+if (auto) {
+  alert('alo')
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
