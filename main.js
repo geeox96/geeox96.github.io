b6e0219bd963f89935c4d119841d4341c76d60e0
Geovanio Carnelosso <geeox96@gmail.com>
2021-10-13 14:15:59 -0300

    new

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const slides = document.querySelectorAll('.slide');
-const next = document.querySelector('#next');
-const prev = document.querySelector('#prev');
-const auto = true; // Auto scroll
-const intervalTime = 5000;
-let slideInterval;
+const auto = true;
+const tempSlide = 5000
 
 const nextSlide = () => {
+  console.log('chamou')
   const current = document.querySelector('.current');
   current.classList.remove('current');
   if (current.nextElementSibling) {
@@ -16,22 +14,4 @@ const nextSlide = () => {
   setTimeout(() => current.classList.remove('current'));
 };
 
-next.addEventListener('click', e => {
-  nextSlide();
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, intervalTime);
-  }
-});
-
-prev.addEventListener('click', e => {
-  prevSlide();
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, intervalTime);
-  }
-});
-if (auto) {
-  alert('alo')
-  slideInterval = setInterval(nextSlide, intervalTime);
-}
+setInterval(nextSlide, tempSlide);
